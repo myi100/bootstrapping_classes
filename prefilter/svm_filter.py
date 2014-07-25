fb41edf6f30a91c975f0a3569f457ed6e5ef8ef0
myi100 <myi100@psu.edu>
2014-07-25 14:53:22 -0400

    formatted 2003 nyt source

--- prefilter/svm_filter.py
+++ prefilter/svm_filter.py
@@ -71,7 +71,7 @@ test_set = []
 for fullfile in filelist:
 	f = open(fullfile).read().split('\n\n\n')
 	test_set = test_set + f
-	# break
+	break
 
 print 'Test-set size: '+str(len(test_set))+'\n'
 ###################################
@@ -85,19 +85,52 @@ elapsed_time = time.time() - start_time
 print 'Classification time: '+str(elapsed_time)
 print 'Frequency:\n'+str(itemfreq(predicted))+'\n'
 
-positive_cases = open('/Users/muhammedyidris/bootstrapping_classes/bootstrap/filtered_nyt.txt', 'w+')
+
+p_fileno = 100
+positive_cases = open('/Users/muhammedyidris/git/bootstrapping_classes/filtered_nyt/filtered_nyt-'+str(p_fileno)+'.txt', 'w+')
 p = 0
-keyword_cases = open('/Users/muhammedyidris/bootstrapping_classes/boostrap/filtered_nyt_by_keyword.txt', 'w+')
+
+k_fileno = 100
+keyword_cases = open('/Users/muhammedyidris/git/bootstrapping_classes/filtered_nyt/by_keyword/filtered_nyt_by_keyword-'+str(k_fileno)+'.txt', 'w+')
 k = 0
+
 keywords = ['protest', 'protesters', 'protests', 'rights', 'demonstrators', 'demonstration', 'support', 'demonstrations', 'activists', 'opposition', 'arrested', 'crowd', 'fired', 'march', 'hundreds', 'marched', 'protesting', 'organized', 'demand', 'protested', 'organization', 'demonstrated', 'demanding', 'dozens', 'criticized', 'protestors', 'activist', 'protester', 'marchers', 'Demonstrators', 'demands', 'oppose', 'boycott', 'demonstrate', 'organizers', 'casualties', 'anti-government', 'criticizing']
+
 
 for i in range(0, len(predicted)):
 	if predicted[i] == 1:
+		if positive_cases.tell() + sys.getsizeof(test_set[i]) > 11141120:
+			positive_cases.close()
+			p_fileno+=1
+			positive_cases = open('/Users/muhammedyidris/git/bootstrapping_classes/filtered_nyt/test/filtered_nyt-'+str(p_fileno)+'.txt', 'w+')
+		
 		print >> positive_cases, test_set[i]+'\n'
 		p+=1
+
 		if any(key in test_set[i][0:300] for key in keywords):
+			if keyword_cases.tell() + sys.getsizeof(test_set[i]) > 11141120:
+				keyword_cases.close()
+				k_fileno+=1
+				keyword_cases = open('/Users/muhammedyidris/git/bootstrapping_classes/test/filtered_nyt/by_keyword/filtered_nyt_by_keyword-'+str(k_fileno)+'.txt', 'w+')
+			
 			print >> keyword_cases, test_set[i]+'\n'
 			k+=1
+
+
+
+# positive_cases = open('/Users/muhammedyidris/bootstrapping_classes/bootstrap/filtered_nyt.txt', 'w+')
+# p = 0
+# keyword_cases = open('/Users/muhammedyidris/bootstrapping_classes/boostrap/filtered_nyt_by_keyword.txt', 'w+')
+# k = 0
+# keywords = ['protest', 'protesters', 'protests', 'rights', 'demonstrators', 'demonstration', 'support', 'demonstrations', 'activists', 'opposition', 'arrested', 'crowd', 'fired', 'march', 'hundreds', 'marched', 'protesting', 'organized', 'demand', 'protested', 'organization', 'demonstrated', 'demanding', 'dozens', 'criticized', 'protestors', 'activist', 'protester', 'marchers', 'Demonstrators', 'demands', 'oppose', 'boycott', 'demonstrate', 'organizers', 'casualties', 'anti-government', 'criticizing']
+
+# for i in range(0, len(predicted)):
+# 	if predicted[i] == 1:
+# 		print >> positive_cases, test_set[i]+'\n'
+# 		p+=1
+# 		if any(key in test_set[i][0:300] for key in keywords):
+# 			print >> keyword_cases, test_set[i]+'\n'
+# 			k+=1
 
 print 'Positive cases: '+str(p)+' ('+str(float(p)/len(test_set) * 100)[0:5]+'% of test-cases.)'
 print 'Keyword-filtered cases: '+str(k)+' ('+str((float(k)/p)* 100)[0:4]+'% of positive-cases, '+str((float(k)/len(test_set))* 100)[0:4]+'% of test-cases)\n'
